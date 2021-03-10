caed615007bc8f8de7cc8debe9b815a666984c3c
Andrey Krainov <metal.awp@gmail.com>
2021-03-10 19:27:33 +0300

    init: start task Typical Arrays Problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
+exports.min = function min(array) {
+    if (array === undefined || array.length == 0) {
+        return 0;
+    } else {
+        let min = array[0];
+        for (let i = 1; i < array.length; i++) {
+            array[i] < min ? (min = array[i]) : false;
+        }
+        return min;
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array === undefined || array.length == 0) {
+        return 0;
+    } else {
+        let max = array[0];
+        for (let i = 1; i < array.length; i++) {
+            array[i] > max ? (max = array[i]) : false;
+        }
+        return max;
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array === undefined || array.length == 0) {
+        return 0;
+    } else {
+        let avg = 0;
+        for (let i = 0; i < array.length; i++) {
+            avg += array[i];
+        }
+        return avg / array.length;
+    }
+};
